3715f4ea8a238fba3de562fb7786c0ff8e86f9a3
Gilbike <marton.dikacz@gmail.com>
2023-09-23 17:58:10 +0200

    feat: :zap: add /set api route

--- src/app/api/set/route.ts
+++ src/app/api/set/route.ts
@@ -1,3 +1,4 @@
+import { DataSets } from "@/data/datasets";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
@@ -5,5 +6,12 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ ok: false, error_code: "missing_param", error: "setid param missing in request" });
   }
 
-  return NextResponse.json({ ok: true, data: { cardFront: "cardBack" } });
+  const id = request.nextUrl.searchParams.get("setid");
+  const set = DataSets.find((x) => x.id === id);
+
+  if (!set) {
+    return NextResponse.json({ ok: false, error_code: "invalid_param", error: "set with id does not exists" });
+  }
+
+  return NextResponse.json({ ok: true, data: set });
 }
