85ab0bd67690baf875089f1450263e6c0f34ab32
Gilbike <marton.dikacz@gmail.com>
2023-09-23 17:51:26 +0200

    feat: :card_file_box: add datasets, add /sets api route

--- src/app/api/sets/route.ts
+++ src/app/api/sets/route.ts
@@ -1,5 +1,6 @@
+import { DataSets } from "@/data/datasets";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json({ ok: true, data: ["testID"] });
+  return NextResponse.json({ ok: true, data: DataSets.map((x) => x.id) });
 }
---
+++ src/data/datasets.ts
@@ -0,0 +1,19 @@
+type DataSet = { [key: string]: string };
+
+const MagyarJelenkor: DataSet[] = require("./mjelenkor.json");
+const Magyar19451956: DataSet[] = require("./m19451956.json");
+
+export const DataSets = [
+  {
+    id: "m19451956",
+    name: "Magyarország 1945-1956",
+    data: Magyar19451956,
+    count: Object.keys(Magyar19451956).length,
+  },
+  {
+    id: "mjelenkor",
+    name: "Magyarország Jelenkor",
+    data: MagyarJelenkor,
+    count: Object.keys(MagyarJelenkor).length,
+  },
+];
