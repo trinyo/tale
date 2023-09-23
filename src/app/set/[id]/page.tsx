f6ebea30ba26c429e1ddd98456076f1f44538cb3
Gilbike <marton.dikacz@gmail.com>
2023-09-23 21:16:28 +0200

    feat: :lipstick: add set page layout with flashcard

--- src/app/set/[id]/page.tsx
+++ src/app/set/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Flashcard from "@/components/Flashcard";
 import { DataSets } from "@/data/datasets";
 import { redirect, useRouter } from "next/navigation";
 import React from "react";
@@ -19,5 +20,17 @@ export default function SetPage({ params }: { params: { id: string } }) {
     redirect("/");
   }
 
-  return <div>{JSON.stringify(set)}</div>;
+  return (
+    <>
+      <h1 className="font-bold text-display">{set.data!.name}</h1>
+      <div className="font-light text-label text-dark-outline">
+        <span className="font-medium">Tip: </span>
+        <div className="p-[1px] rounded bg-dark-2-normal h-7 w-7 inline-block mr-1">
+          <div className="rounded-[3px] bg-dark-3-normal h-[93%] flex justify-center items-center">F11</div>
+        </div>
+        for better focus
+      </div>
+      <Flashcard data={set.data!.data} />
+    </>
+  );
 }
