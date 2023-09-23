b68b748ac340befc062d8baa4ced3c7f0f992866
Gilbike <marton.dikacz@gmail.com>
2023-09-23 19:22:02 +0200

    feat: :lipstick: add navigation to set page

--- src/components/SetView.tsx
+++ src/components/SetView.tsx
@@ -9,7 +9,7 @@ interface IProps {
 
 export default function SetView({ name, count, id }: IProps) {
   return (
-    <Link href="/" className="bg-dark-1-normal hover:bg-dark-1-hover rounded-8 p-3 flex flex-col">
+    <Link href={`/set/${id}`} className="bg-dark-1-normal hover:bg-dark-1-hover rounded-8 p-3 flex flex-col">
       <h6 className="font-medium text-body2">{name}</h6>
       <div className="font-light text-dark-outline text-label">{count} card</div>
     </Link>
